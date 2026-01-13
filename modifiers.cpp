533a59e27cbaa984ec949124039b23f67622e462
Saniya Mulla <saniyamulla2105@gmail.com>
2026-01-13 14:18:38 +0000

    Access Modifiers

---
+++ modifiers.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Teacher{
+     //properties//attributes
+private:
+     double salary;
+public:
+     string name;
+     string dept;
+     string subject;
+
+     //methods// member functions
+     void changeDept(string newDept){
+        dept= newDept;
+     }
+     //setter
+     void setSalary(double s){
+          salary=s;
+     }
+     //getter
+     double getSalary(){
+          return salary;
+     }
+};
+int main(){
+     Teacher t1;
+     t1.name="saniya";
+     t1.subject="C++";
+     t1.dept="AIDS";
+     t1.setSalary(25000);
+
+     cout<<t1.name<<endl;
+     cout<<t1.getSalary()<<endl;
+     return 0;
+
+}
